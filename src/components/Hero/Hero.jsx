9d74ec1f787dd4b6ac9bc66959552e7a09949aa7
Roberta D'Agostino <robertadag02@gmail.com>
2022-08-19 20:08:29 +0200

    feat:responsive hamburger menu

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,13 @@ const Hero = () => {
     <div className="Hero__img-container">
         <img className="Hero__img" src={require('./foto.jpg') } alt="me" />
     </div>
-  </div>;
+    <div className="Hero__text">
+      <h1>Hi! <br /> Welcome to my WebSite!</h1>
+      <h2>I'm <span className="Hero__text-underline"> Roberta D'Agostino </span> and I'm a junior Frontend Developer.</h2>
+
+      <p>Let's scroll down to read my Story!</p>
+    </div>
+  </div>
 };
 
 export default Hero;
