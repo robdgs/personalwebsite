3eec8aab949a437ffcce6ba3b4264b7d4b55a70f
Roberta D'Agostino <robertadag02@gmail.com>
2022-08-19 19:11:26 +0200

    fix: responsiveness improved on Navbar component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,11 @@
 import "./index.css";
 
 const Hero = () => {
-  return <div className="Hero">
+  return <div className="Hero" >
     <div className="Hero__spinner"></div>
     <div className="Hero__img-container">
         <img className="Hero__img" src={require('./foto.jpg') } alt="me" />
     </div>
-
-
   </div>;
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import './index.css';
 
 const Navbar = ()=> {
+ 
     return (
         <div className="Navbar">
             <ul className="Navbar__list">
@@ -9,7 +10,27 @@ const Navbar = ()=> {
             <a className="Navbar__list-link" href="https://github.com/robdgs" target="_blank">GITHUB</a>
             <a className="Navbar__list-link" href="#contactme">CONTACT ME</a>
           </ul>
+
+          <div className="navbar" >
+        <div className="container nav-container">
+            <input className="checkbox" type="checkbox" name="" id="" />
+            <div className="hamburger-lines">
+              <span className="line line1"></span>
+              <span className="line line2"></span>
+              <span className="line line3"></span>
+            </div>  
+       
+          <div className="menu-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">blogs</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a href="#">contact</a></li>
+          </div>
         </div>
+      </div>
+        </div>
+        
     )
        
 }
