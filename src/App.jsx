189a64adf9ada36f656fa21e18f82c7aef213880
Roberta D'Agostino <robertadag02@gmail.com>
2022-08-19 15:58:08 +0200

    feat:Hero component animation created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Navbar from "./components/Navbar";
 import './App.css';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+const Hero = () => {
+  return <div className="Hero">
+    <div className="Hero__spinner"></div>
+    <div className="Hero__img-container">
+        <img className="Hero__img" src={require('./foto.jpg') } alt="me" />
+    </div>
+
+
+  </div>;
+};
+
+export default Hero;
