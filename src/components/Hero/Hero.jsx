585ec6502e2e794232e50895b590f79a496cac75
Roberta D'Agostino <robertadag02@gmail.com>
2022-08-20 14:10:59 +0200

    fix:responsiveness on Hero and Overview

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,15 +2,18 @@ import "./index.css";
 
 const Hero = () => {
   return <div className="Hero" >
+    <div className="Hero__spinner-container">
     <div className="Hero__spinner"></div>
+   
     <div className="Hero__img-container">
         <img className="Hero__img" src={require('./foto.jpg') } alt="me" />
+    </div>
     </div>
     <div className="Hero__text">
       <h1>Hi! <br /> Welcome to my WebSite!</h1>
       <h2>I'm <span className="Hero__text-underline"> Roberta D'Agostino </span> and I'm a junior Frontend Developer.</h2>
 
-      <p>Let's scroll down to read my Story!</p>
+      {/* <p>Let's scroll down to read my Story!</p> */}
     </div>
   </div>
 };
